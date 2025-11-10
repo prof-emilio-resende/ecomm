186a37889210133075e8342dec82d9827949995c
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2025-11-10 20:52:41 -0300

    add product into catalog

--- src/__tests__/services/catalog.service.test.ts
+++ src/__tests__/services/catalog.service.test.ts
@@ -12,10 +12,29 @@ describe('Catalog Service', () => {
                 // Act
                 const catalog = catalogService.list();
 
-                
                 // Assert
                 expect(catalog).toEqual(expectedCatalog);
                 expect(catalog).toHaveLength(0);
+            });
+        });
+
+        describe('when catalog has products', () => {
+            it('should return an array of products', () => {
+                // Arrange
+                const expectedP1 = new Product('Product 1', 10.0);
+                const expectedP2 = new Product('Product 2', 20.0);
+                
+                const expectedCatalog = [expectedP1, expectedP2];
+
+                // Act
+                const catalogService = new CatalogService();
+                catalogService.add(expectedP1);
+                catalogService.add(expectedP2);
+                const catalog = catalogService.list();
+
+                // Assert
+                expect(catalog).toEqual(expectedCatalog);
+                expect(catalog).toHaveLength(2);
             });
         });
     });
--- src/services/catalog.service.ts
+++ src/services/catalog.service.ts
@@ -1,9 +1,15 @@
 import { Product } from "../models/product";
 
 export class CatalogService {
+    private catalog: Array<Product> = [];
+
     constructor() {}
 
+    add(product: Product): void {
+        this.catalog.push(product);
+    }
+
     list(): Array<Product> {
-        return [];
+        return this.catalog;
     }
 }
