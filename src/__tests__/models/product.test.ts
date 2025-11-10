70d93492ef0ed21f863031b1f41d04dc434b47ad
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2025-11-10 20:04:04 -0300

    tests produto

---
+++ src/__tests__/models/product.test.ts
@@ -0,0 +1,15 @@
+import { Product } from '../../models/product';
+
+describe('Product Model', () => {
+  it('should create a product with name and price', () => {
+    // Arrange
+    const name = "Produto Teste";
+    const price = 100.00;
+    // Act
+    const product = new Product(name, price);
+
+    // Assert
+    expect(product.name).toBe(name);
+    expect(product.price).toBe(price);
+  });
+});
